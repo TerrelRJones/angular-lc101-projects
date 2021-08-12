add21638ce81b6532ba07f412f0b71be7392c43b
Terrel <TerrelRJones@gmail.com>
2021-08-11 19:33:32 -0700

    worked through half part1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'fav-photos',
-  templateUrl: './fav-photos.component.html',
-  styleUrls: ['./fav-photos.component.css']
+  selector: "fav-photos",
+  templateUrl: "./fav-photos.component.html",
+  styleUrls: ["./fav-photos.component.css"],
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = "PHOTOS!";
+  image1 =
+    "https://summitsports.scene7.com/is/image/SummitSports/571747_571755_1?$600$";
+  image2 =
+    "https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/ddba44b6-bf0f-4362-b0be-20c68161f8eb/air-jordan-1-pollen-release-date.jpg";
+  image3 =
+    "https://images.immediate.co.uk/production/volatile/sites/21/2020/04/Sentinel-full-bike-7c2bc57.jpg?quality=90&resize=620,413";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
