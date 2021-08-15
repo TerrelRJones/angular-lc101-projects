98734f623204166188a8c6e80cb1eaf8d710417e
Terrel <TerrelRJones@gmail.com>
2021-08-15 11:15:09 -0700

    lesson 2 exercises done

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,15 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+
+   addMovie (newTitle: string) {
+
+      if(newTitle === ""){
+         alert('Must enter a title')
+      }
+
+      if(!this.movies.includes(newTitle)){
+      this.movies.push(newTitle)
+   }
+   }
 }
